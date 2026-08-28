a51a1fc84e0829a18e118f3c8991a038261bdf53
haz <57262649+haz7799@users.noreply.github.com>
2026-08-28 05:43:24 +0000

    fix: add postcss config to compile tailwind css

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,10 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // 請在此填入 Firebase Console 提供的設定檔
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: "AIzaSyDFqi8kJi15SOQ63FTOjhYQ5vfVdE2lR54",
+  authDomain: "incircle-pwa.firebaseapp.com",
+  projectId: "incircle-pwa",
+  storageBucket: "incircle-pwa.firebasestorage.app",
+  messagingSenderId: "1029221290070",
+  appId: "1:1029221290070:web:9937347e2ff2994bacabf7"
 };
 
 // 避免在 Next.js 開發環境中重複初始化
